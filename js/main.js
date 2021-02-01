077d12a7e7c5094c8e534d5db82582bbb6e5f9a8
Sangeetha543 <78066086+Sangeetha543@users.noreply.github.com>
2021-02-01 15:46:26 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-fetch("../data/data.json").then(response=>{
+fetch("https://sangeetha543.github.io/ecommerce/data/data.json").then(response=>{
 	return response.json()
 }).then(data=>{
 	console.log(data)
